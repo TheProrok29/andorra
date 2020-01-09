793bd51f24729b8d13d79059a52d5e0ee2246385
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-01-09 10:57:44 +0100

    Add statistics url to navbar
    * move statistics template to players app

--- players/tests.py
+++ players/tests.py
@@ -1,3 +1,9 @@
-# from django.test import TestCase
+from django.test import TestCase, override_settings
 
-# Create your tests here.
+
+@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+class HomePageTest(TestCase):
+
+    def test_uses_home_template(self):
+        response = self.client.get('/statistics/')
+        self.assertTemplateUsed(response, 'statistics.html')
