6921be6b300c3afdacc23711e3c1097fa2a5163d
Maniek5k <emailer5k@gmail.com>
2020-01-10 19:00:12 +0000

    Task 7

--- journeys/models.py
+++ journeys/models.py
@@ -1,3 +1,7 @@
-# from django.db import models
-#
-# # Create your models here.
+from django.db import models
+from datetime import datetime
+from datetime import timedelta
+
+
+class ActiveJourney(models.Model):
+    end_date = datetime.now() + timedelta(seconds=20)
