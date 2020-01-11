8320ae588aa63c97343f28bfcd2cde90e953fc5c
Tomasz Szatkowski <szatkus+gitlab@gmail.com>
2020-01-11 15:35:59 +0100

    #5 add middleware test

--- characters/tests.py
+++ characters/tests.py
@@ -1,4 +1,7 @@
-from django.test import TestCase, override_settings
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.test import TestCase, override_settings, RequestFactory
+
+from .middleware import InceptionMiddleware
 
 
 @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
@@ -7,3 +10,16 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/statistics/')
         self.assertTemplateUsed(response, 'statistics.html')
+
+
+class MiddlewareTest(TestCase):
+
+    def test_character_creation(self):
+        sut = InceptionMiddleware(lambda r: None)
+        request_factory = RequestFactory()
+        session_middleware = SessionMiddleware()
+        request = request_factory.get('/')
+        session_middleware.process_request(request)
+        sut(request)
+        self.assertIsNotNone(request.character)
+        self.assertIsNot(request.character.name, '')
