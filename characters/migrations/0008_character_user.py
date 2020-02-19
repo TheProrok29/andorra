5e2ee69c95168fb9483890cb36b2a151f451441c
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-02-19 08:45:58 +0100

    Flake8 fix

--- characters/migrations/0008_character_user.py
+++ characters/migrations/0008_character_user.py
@@ -16,6 +16,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='character',
             name='user',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL, verbose_name='User'),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE,
+                                    to=settings.AUTH_USER_MODEL, verbose_name='User'),
         ),
     ]
