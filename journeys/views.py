c5296407b5ea45740e9cce90f81fb0d61d8a8c46
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-02-24 19:26:07 +0100

    Add last journey info to journey template_context

--- journeys/views.py
+++ journeys/views.py
@@ -13,24 +13,38 @@ from .models import ActiveJourney
 class JourneyView(View):
 
     def get(self, request):
+        template_context = {}
         active_journey = ActiveJourney.objects.filter(character=request.character, active=True).first()
-        return render(request, 'journeys.html', {'journeys': journey_list,
-                                                 'active_journey': active_journey,
-                                                 'journey_disabled': request.character.busy,
-                                                 })
+        last_journey = ActiveJourney.objects.filter(character=request.character, active=False).first()
+        template_context = {'journeys': journey_list,
+                            'active_journey': active_journey,
+                            'last_journey': last_journey,
+                            'journey_disabled': request.character.busy}
+        if last_journey is not None:
+            definition: JourneyDefinition = next(filter(lambda j: j.slug == last_journey.slug, journey_list))
+            log = list(definition.proceed(request.character))
+            print('Nazwa: ', definition.name)
+            template_context['last_journey_name'] = definition.name
+            template_context['last_journey_log'] = log
+
+        return render(request, 'journeys.html', template_context)
 
     def post(self, request):
         if len(ActiveJourney.objects.filter(character=request.character, active=True)) > 0:
             return HttpResponseServerError(status=500)
         ActiveJourney.objects.filter(character=request.character).delete()
         journey_id = request.POST['journey_id']
+        print(journey_id)
         definition: JourneyDefinition = next(filter(lambda j: j.slug == journey_id, journey_list))
+        print(definition.name)
         log = list(definition.proceed(request.character))
+        print(log)
         duration = sum([l.duration for l in log])
 
         ActiveJourney(
             character=request.character,
             end_date=timezone.now() + timedelta(seconds=duration),
-            log=log
+            log=log,
+            slug=journey_id,
         ).save()
         return redirect('journeys')
