1418e323778b05216d8019d46a0630116e646b5f
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-02-24 18:16:00 +0100

    Add second journey definition

--- journeys/definitions.py
+++ journeys/definitions.py
@@ -27,6 +27,17 @@ class FirstJourney(JourneyDefinition):
         yield JourneyEvent(5, 'second')
 
 
+class SecondJourney(JourneyDefinition):
+    slug = 'second_journey'
+    name = 'Enter the dark forest'
+
+    def proceed(self, character: Character) -> JourneyEvent:
+        yield JourneyEvent(5, 'first')
+        yield JourneyEvent(10, 'fight')
+        yield JourneyEvent(5, 'second')
+
+
 journey_list = [
-    FirstJourney()
+    FirstJourney(),
+    SecondJourney(),
 ]
