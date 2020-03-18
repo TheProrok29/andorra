6be56bf1084722b9171a704bd1bc237bad78a822
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-18 19:03:43 +0100

    Add start_date field to ActiveJoureny model

--- journeys/models.py
+++ journeys/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 from picklefield.fields import PickledObjectField
-
 from characters.models import Character
 
 
 class ActiveJourney(models.Model):
     character = models.OneToOneField(Character, CASCADE)
+    start_date = models.DateTimeField(blank=True, null=True)
     end_date = models.DateTimeField()
     log = PickledObjectField()
     active = models.BooleanField(default=True)
