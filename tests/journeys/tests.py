e5a282e2eb05fe289641d33210e96993fb951285
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-18 19:05:20 +0100

    Add failing function test show_finished_journey_event_step

--- tests/journeys/tests.py
+++ tests/journeys/tests.py
@@ -1,9 +1,11 @@
 from datetime import timedelta
-from unittest.mock import patch
-
+from unittest.mock import patch, Mock
+import datetime
 from django.test import TestCase, override_settings
 from bs4 import BeautifulSoup
 from django.utils import timezone
+from journeys.models import ActiveJourney, show_finished_journey_event_step
+from journeys.definitions import JourneyEvent
 
 
 @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
@@ -28,3 +30,28 @@ class JourneysTest(TestCase):
             response = self.client.get('/journeys/')
             soup = BeautifulSoup(response.content, features='html.parser')
             self.assertIsNone(soup.select_one('.countdown'))
+
+    def test_show_finished_journey_event_step_if_two_event_finished(self):
+        log = [JourneyEvent(duration=5, description='first'), JourneyEvent(
+            duration=10, description='fight'), JourneyEvent(duration=5, description='second')]
+        start_date = datetime.datetime(2015, 6, 15, 14, 45, 9, 182703)
+        now_date = datetime.datetime(2015, 6, 15, 14, 45, 26, 182703)
+        end_date = datetime.datetime(2015, 6, 15, 14, 45, 29, 182703)
+        journey = Mock(spec=ActiveJourney.objects)
+        journey.start_date = start_date
+        journey.end_date = end_date
+        journey.log = log
+        self.assertEqual([JourneyEvent(duration=5, description='first'), JourneyEvent(
+            duration=10, description='fight')], show_finished_journey_event_step(journey, now_date))
+
+    def test_show_finished_journey_event_step_if_no_event_finished(self):
+        log = [JourneyEvent(duration=5, description='first'), JourneyEvent(
+            duration=10, description='fight'), JourneyEvent(duration=5, description='second')]
+        start_date = datetime.datetime(2015, 6, 15, 14, 45, 9, 182703)
+        now_date = datetime.datetime(2015, 6, 15, 14, 45, 11, 182703)
+        end_date = datetime.datetime(2015, 6, 15, 14, 45, 29, 182703)
+        journey = Mock(spec=ActiveJourney.objects)
+        journey.start_date = start_date
+        journey.end_date = end_date
+        journey.log = log
+        self.assertEqual([], show_finished_journey_event_step(journey, now_date))
