6e704967e00625cc2c674e467cb50f818a9d6f14
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-18 19:06:16 +0100

    Add show_finished_journey_event_step implementation

--- journeys/models.py
+++ journeys/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 from picklefield.fields import PickledObjectField
+from django.utils import timezone
+from datetime import timedelta
 from characters.models import Character
 
 
@@ -20,3 +22,15 @@ class ActiveJourney(models.Model):
 def toggle_journey_log_visibility(journey: ActiveJourney):
     journey.is_visible = not journey.is_visible
     journey.save()
+
+
+def show_finished_journey_event_step(journey: ActiveJourney, now_date: timezone) -> ActiveJourney.log:
+    log_part = []
+    total = 0
+    if now_date > journey.end_date:
+        return journey.log
+    for i, _ in enumerate(journey.log):
+        total += journey.log[i].duration
+        if now_date > journey.start_date + timedelta(seconds=total):
+            log_part.append(journey.log[i])
+    return log_part
