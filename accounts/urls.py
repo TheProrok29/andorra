36117936a62a123a43ac53a5f5e873fc579bf1d1
Tomasz Małecki <tomaszm225@gmail.com>
2020-03-18 18:11:56 +0000

    #23 change pass

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordChangeView
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
-
+    path('password/', PasswordChangeView.as_view(template_name='registration/password_change.html',
+                                                 success_url=reverse_lazy('home')), name='PasswordChange'),
 ]
