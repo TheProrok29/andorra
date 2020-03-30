4983c790975932a3a9ec316556c8604e46a19a69
wiktoria <jabwik@gmail.com>
2020-03-30 23:51:27 +0200

    Flake fixes.

--- tests/mechanics/test_fight.py
+++ tests/mechanics/test_fight.py
@@ -8,7 +8,9 @@ from mechanics.fight import Fight
 class FightTest(TestCase):
     def test_fight(self):
         random.seed(2137)
-        hero = Character(name='jan', level=100, health_points=10, strength=5, defense_strength=3, reflex=3)  # strength = attack
+        hero = Character(
+            name='jan', level=100, health_points=10, strength=5, defense_strength=3, reflex=3
+        )  # strength = attack
         enemy = Character(name='wilk', level=5, health_points=5, strength=1, defense_strength=2, reflex=2)
 
         for _ in range(100):
